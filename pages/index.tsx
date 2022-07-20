3c3de31e406843200a29b4b94f0e0cd979a78463
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-07-21 03:17:04 +0900

    chore: components folder out at root

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next'
-import { Audits } from './components/Audits'
-import { Ecosystem } from './components/Ecosystem'
-import { Footer } from './components/Footer'
-import { Header } from './components/Header'
-import { Hero } from './components/Hero'
-import { Partners } from './components/Partners'
-import { Work } from './components/Work'
+import { Audits } from '../components/Audits'
+import { Ecosystem } from '../components/Ecosystem'
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+import { Hero } from '../components/Hero'
+import { Partners } from '../components/Partners'
+import { Work } from '../components/Work'
 
 const Home: NextPage = () => {
   return (
