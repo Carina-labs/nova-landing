0c2b4e14b2b38e277c22c99433044742bdcf8e1e
boosik <103121868+boosik@users.noreply.github.com>
2022-07-21 05:29:06 +0000

    Deploying to gh-pages from @ Carina-labs/nova-landing@9fdec3c0cbf63eeac669b7562fe3ba2bb7a742cf 🚀

---
+++ _next/static/hiJrhjFwotJkRQon_K_73/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-bd5831bb8d807736.js"],"/_error":["static/chunks/pages/_error-cd38dd4ccec7f2af.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
