362b47c995bfba99d015d32d1e38933cdc968adc
boosik@a41ventures.com <boosik@a41ventures.com>
2022-07-29 13:54:38 +0900

    fix: add basePath in next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "",
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    loader: 'akamai',
-    path: '/'
+    loader: "akamai",
+    path: "/"
   }
 }
 
