3a0df76443b5c01857fc2b5cdac7de2a825ce7eb
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-08-08 21:33:39 +0900

    chore: add mp4 props playsInline

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
   return (
     <div className="relative h-auto ">
       <div className="z-[-1] h-[1795px] absolute top-[2.2rem] w-full">
-        <video className="w-full" autoPlay muted loop>
+        <video className="w-full" autoPlay muted loop playsInline>
           <source src="/assets/landing_1920_high.mp4" type="video/mp4" />
         </video>
       </div>
