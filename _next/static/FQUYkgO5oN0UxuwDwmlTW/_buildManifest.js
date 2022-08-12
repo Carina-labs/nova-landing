f5ab452d4f1ac3dff0d90ad0af97b579bfa8b9e5
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2022-08-12 11:02:36 +0000

    Deploying to gh-pages from @ Carina-labs/nova-landing@1737e7c68f646b472c4f0dc112b63f8dcfd44537 🚀

---
+++ _next/static/FQUYkgO5oN0UxuwDwmlTW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/535-0cc4884340e4288b.js","static/chunks/pages/index-4d54d58899239d5b.js"],"/_error":["static/chunks/pages/_error-cd38dd4ccec7f2af.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
