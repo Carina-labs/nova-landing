59fcb0d46c3d92c4c4aed9fef9677319217dca1d
추교현 <scottchoo@chugyohyeon-ui-MacBookPro.local>
2022-08-25 11:38:35 +0900

    fix: mobile ui & emai subscription message

--- components/Ecosystem.tsx
+++ components/Ecosystem.tsx
@@ -18,51 +18,51 @@ export const Ecosystem = () => {
   return (
     <div
       id="down"
-      className="px-[1.7rem] md:block relative z-20 mt-[5.2rem] md:mt-[16rem] mb-[5.4rem] md:mb-[20rem] overflow-hidden max-w-[1050px] mx-auto"
+      className="md:block relative z-20 mt-[5.2rem] md:mt-[16rem] mb-[5.4rem] md:mb-[20rem] overflow-hidden max-w-[780px] lg:max-w-[1050px] mx-auto"
     >
       <h1 className="text-tw-white font-extrabold text-center text-24 md:text-32 lg:text-40 xl:text-48 2xl:text-62 3xl:text-64">
         Supernova Ecosystem
       </h1>
       <div className="w-full mx-auto">
-        <dl className="mt-[1.5rem] md:mt-[4.5rem] py-2 lg:flex lg:justify-center">
+        <dl className="mx-[1.3rem] mt-[1.5rem] md:mt-[4.5rem] py-2 md:flex md:justify-center">
           {/* ATOM */}
-          <div className="md:w-1/3 h-[140px] lg:h-[180px] mx-auto px-6 lg:mx-2 mb-[1.1rem] flex border-2 border-yellow-default bg-white shadow rounded-2xl overflow-hidden">
+          <div className="md:w-1/3 h-[140px] lg:h-[180px] mx-auto px-6 mb-[1.1rem] flex border-2 border-yellow-default bg-white shadow rounded-2xl overflow-hidden">
             <div className="w-1/3 pr-4 flex items-center">
               <img className="w-[80px] h-[80px]" src="/assets/ATOM.svg" />
             </div>
-            <div className="w-2/3 my-auto">
-              <dt className="text-left font-bold text-gray-500 truncate text-20 xl:text-28">
+            <div className="w-2/3 my-auto md:mt-[25px] lg:mt-[40px]">
+              <dt className="text-left font-bold text-gray-500 truncate text-20 xl:text-24">
                 ATOM
               </dt>
-              <dd className="mt-1 text-left font-medium text-black text-10 xl:text-14">
+              <dd className="mt-1 text-left font-medium text-black text-10 lg:text-12">
                 Cosmos is the Internet of Blockchains.
               </dd>
             </div>
           </div>
           {/* OSMO */}
-          <div className="md:w-1/3 h-[140px] lg:h-[180px] mx-auto px-6 lg:mx-2 mb-[1.1rem] flex border-2 border-yellow-default bg-white shadow rounded-2xl overflow-hidden">
+          <div className="md:w-1/3 h-[140px] lg:h-[180px] mx-auto px-6 md:mx-3 mb-[1.1rem] flex border-2 border-yellow-default bg-white shadow rounded-2xl overflow-hidden">
             <div className="w-1/3 pr-4 flex items-center">
               <img className="w-[80px] h-[80px]" src="/assets/OSMO.svg" />
             </div>
-            <div className="w-2/3 my-auto">
-              <dt className="text-left font-bold text-gray-500 truncate text-20 xl:text-28">
+            <div className="w-2/3 my-auto md:mt-[25px] lg:mt-[40px]">
+              <dt className="text-left font-bold text-gray-500 truncate text-20 xl:text-24">
                 OSMO
               </dt>
-              <dd className="mt-1 text-left font-medium text-black text-10 xl:text-14">
+              <dd className="mt-1 text-left font-medium text-black text-10 lg:text-12">
                 A cross-chain automated market maker(AMM) with the Cosmos SDK.
               </dd>
             </div>
           </div>
           {/* JUNO */}
-          <div className="md:w-1/3 h-[140px] lg:h-[180px] mx-auto px-6 lg:mx-2 mb-[1.1rem] flex border-2 border-yellow-default bg-white shadow rounded-2xl overflow-hidden">
+          <div className="md:w-1/3 h-[140px] lg:h-[180px] mx-auto px-6 mb-[1.1rem] flex border-2 border-yellow-default bg-white shadow rounded-2xl overflow-hidden">
             <div className="w-1/3 pr-4 flex items-center">
               <img className="w-[80px] h-[80px]" src="/assets/JUNO.svg" />
             </div>
-            <div className="w-2/3 my-auto">
-              <dt className="text-left font-bold text-gray-500 truncate text-20 xl:text-28">
+            <div className="w-2/3 my-auto md:mt-[25px] lg:mt-[40px]">
+              <dt className="text-left font-bold text-gray-500 truncate text-20 xl:text-24">
                 JUNO
               </dt>
-              <dd className="mt-1 text-left font-medium text-black text-10 xl:text-14">
+              <dd className="mt-1 text-left font-medium text-black text-10 lg:text-12">
                 A decentralized, public, permission-less network for cross-chain
                 smart contracts.
               </dd>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import { Work } from 'components/Work'
 
 const Home: NextPage = () => {
   return (
-    <div className="relative h-auto ">
+    <div className="relative h-auto">
       <div className="z-[-1] h-[1795px] absolute top-[2.2rem] w-full">
         <video className="w-full" autoPlay muted loop playsInline>
           <source src="/assets/landing_1920_high.mp4" type="video/mp4" />
