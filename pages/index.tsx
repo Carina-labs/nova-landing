d70c9f7eb625983be20fd69e522394345d9cb6a0
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-01-30 23:54:32 +0900

    feat: add OG Meta Tag (#21)
    - 랜딩페이지에 OG Meta Tag를 추가함
    - 이미지는 임의로 넣었고 디자이너가 만들어주시면 추가할 예정
    - Vercel 환경변수 추가 | `NEXT_PUBLIC_DOMAIN_ADDRESS` : http://www.supernova.ac

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,30 @@ import { Header } from '../components/Header'
 import { Hero } from '../components/Hero'
 import { Ecosystem } from 'components/Ecosystem'
 import { Work } from 'components/Work'
+import Head from 'next/head'
 
+const imgPath = `${process.env.NEXT_PUBLIC_DOMAIN_ADDRESS}/assets/og.png`
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Home | Supernova</title>
+        <meta
+          name="description"
+          content="Ultimate platform for the staked assets"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Home | Supernova" />
+        <meta
+          property="og:description"
+          content="Ultimate platform for the staked assets"
+        />
+        <meta property="og:site_name" content="Supernova" />
+        <meta property="og:image" content={imgPath} />
+        <meta property="og:image:width" content="2401" />
+        <meta property="og:image:height" content="1261"></meta>
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <div className="relative h-auto">
         <div className="z-[-1] h-[1795px] absolute top-[2.2rem] w-full">
           <video className="w-full" autoPlay muted loop playsInline>
